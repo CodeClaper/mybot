5e92bd55fd5800d9840500bbfb3ec878b57b53d5
zhouchao03 <jerryzhou.zc@gmail.com>
2026-03-03 15:13:43 +0800

    feat: add queue.

--- mybot/cli/bus/queue.py
+++ mybot/cli/bus/queue.py
@@ -1,4 +1,28 @@
+import asyncio
+
+from mybot.cli.bus.message import InboundMessage, OutboundMessage
 
 class MessageBus:
+    """
+    Async message bus that decouples chat channels from the agent.
+    """
     def __init__(self) -> None:
-        pass
+        self.inboud: asyncio.Queue[InboundMessage] = asyncio.Queue()
+        self.outboud: asyncio.Queue[OutboundMessage] = asyncio.Queue()
+
+
+    async def publish_inbound(self, msg: InboundMessage) -> None:
+        """Publish a meesage from a channle to the agent."""
+        await self.inboud.put(msg)
+
+    async def consume_inbound(self) -> InboundMessage:
+        """Consume the next inboud message(block if queue is empty)."""
+        return await self.inboud.get()
+
+    async def publish_outbound(self, msg: OutboundMessage) -> None:
+        """Publish a response from a agent to the channels."""
+        await self.outboud.put(msg)
+
+    async def consume_outbound(self) -> OutboundMessage:
+        """Consume the next outboud message(block if queue is empty)."""
+        return await self.outboud.get()
